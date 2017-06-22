530bbd9b4a2d8d51b41a52e54457d1579e4c0202
okocsis90 <o.kocsis90@gmail.com>
2017-06-22 15:42:21 +0200

    search engine finished, and works

---
+++ SeekAndArchive/SeekAndArchive/FileSearchEngine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class FileSearchEngine
+    {
+        private List<FileInfo> allPaths;
+        public string FileName { get; set; }
+        public string Path { get; set; }
+
+        public FileSearchEngine(string fileName, string path)
+        {
+            FileName = fileName;
+            Path = path;
+        }
+
+        public List<FileInfo> Search()
+        {
+            allPaths = new List<FileInfo>();
+            SearchEngine(Path);
+            if (allPaths.Count == 0)
+            {
+                Console.WriteLine("The specified file does not exist.");
+            }
+            return allPaths;
+        }
+
+        private void SearchEngine(string path)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            if (!dirInfo.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                return;
+            }
+
+            foreach (FileInfo fileInfo in dirInfo.GetFiles())
+            {
+                if (fileInfo.Name == FileName)
+                {
+                    allPaths.Add(fileInfo);
+                }
+            }
+
+            foreach (DirectoryInfo currentDirInfo in dirInfo.GetDirectories())
+            {
+                SearchEngine(currentDirInfo.FullName);
+            }
+        }
+    }
+}
---
+++ SeekAndArchive/SeekAndArchive/SeekAndArchive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class SeekAndArchive
+    {
+        private static List<FileInfo> resultFiles;
+
+        static void Main(string[] args)
+        {
+            FileSearchEngine fileSearchEngine = new FileSearchEngine(args[0], args[1]);
+            resultFiles = fileSearchEngine.Search();
+
+            foreach (FileInfo fileInfo in resultFiles)
+            {
+                Console.WriteLine(fileInfo.FullName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
